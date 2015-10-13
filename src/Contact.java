622c0cf4cb07d23c66fdd18a4a654cc9ea739dd3
JackNel <jcneligan@gmail.com>
2015-10-13 10:38:58 -0400

    Add test code to practice constructors and data structures

--- src/Contact.java
+++ src/Contact.java
@@ -2,33 +2,14 @@
  * Created by Jack on 10/7/15.
  */
 public class Contact {
-    public String name;
+    public String name;  //field
     public String address;
     public String email;
-    public int age;
+    public int age;  //field
 
-    public Contact() {  // Object Constructor
-        this.name = "Zach Oakes";
-        this.email = "zach@ironyard.com";
 
-    }
-
-    public Contact(String name, String address, String email, int age) {
-        this.name = name;
-        this.address = address;
-        this.email = email;
+    public Contact(String name, int age) {  //CLass constructor
+        this.name = name;  //"this.name" refers to the field above.  "= name" refers to the argument passed into the class constructor.
         this.age = age;
-    }
-
-    public String getName() {   //Retrieve the name of an object
-        return name;
-    }
-
-    public void setName(String name) {  //set the name of an object
-        this.name = name;
-    }
-
-    public static boolean isValidName(String name) {
-        return name.length() != 0;
     }
 }
