e04a91c2a79d6d0b06239e29dd53fcac193ed61e
JackNel <jcneligan@gmail.com>
2015-10-13 10:25:20 -0400

    Add test code to practice constructors and data structures

---
+++ src/Contacts.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Jack on 10/13/15.
+ */
+public class Contacts { //This will be a class that used the class Contact, this is a common naming convention
+    public static void main(String[] args) {
+        Contact bob = new Contact("Bob", 30);
+        Contact alice = new Contact("Alice", 20);
+        Contact charlie = new Contact("Charlie", 25);
+
+        ArrayList<Contact> contacts = new ArrayList();  //Create an ArrayList called contacts
+        contacts.add(bob);
+        contacts.add(alice);
+        contacts.add(charlie);
+
+        ArrayList<Contact> contactsUnderThirty = new ArrayList();
+
+        Contact contactToFind = null;
+        for (Contact c : contacts) {  //"Contact c" creates a temporary variable.  contacts refers to the ArrayList named "contacts"
+            if (c.age < 30) {
+                contactsUnderThirty.add(c);
+            }
+        }
+
+        System.out.println(contactsUnderThirty);
+
+        HashMap<String, Contact> contactMap = new HashMap();  // Do the same thing with a HashMap
+        contactMap.put("Bob", bob);
+        contactMap.put("Alice", alice);
+        contactMap.put("Charlie", charlie);
+
+       // contactToFind = contactMap.get("Alice");
+
+        if (contactToFind == null) {
+            System.out.println("Couldn't find contact.");
+        } else {
+            System.out.println("Found Contact!");
+        }
+    }
+}
