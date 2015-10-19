b52f4622efe11633e934dca6a4efd60071c380ca
JackNel <jcneligan@gmail.com>
2015-10-19 10:23:27 -0400

    Add HashMap with a nested ArrayList

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Jack on 10/19/15.  Example of a nested for loop.
@@ -6,14 +7,24 @@ import java.util.ArrayList;
 public class Exercise3 {
     public static void main(String[] args) {
         String[] names = {"Alice", "Bob", "Charlie", "David"};
-        ArrayList<String> twoNames = new ArrayList();
+        HashMap<String, ArrayList<Contact>> people = new HashMap();
 
         for (String name : names) {
             for (String name2 : names) {
                 if (!name.equals(name2)) {
-                    System.out.println(String.format("%s, %s", name, name2));
+                    Contact c = new Contact(name2, 0);
+                    ArrayList<Contact> list = people.get(name);
+                    if (list == null) {
+                        list = new ArrayList();
+                        list.add(c);
+                        people.put(name, list);
+                    } else {
+                        list.add(c);
+                    }
                 }
             }
         }//for loop
+
+        System.out.println();
     }//main method
 }//class Exercise3
