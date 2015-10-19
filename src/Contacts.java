3ffab52275a85fba878e7e4a897c3fd4a2e1910d
JackNel <jcneligan@gmail.com>
2015-10-19 11:47:16 -0400

    Implement comparable interface

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 /**
@@ -9,11 +10,15 @@ public class Contacts { //This will be a class that used the class Contact, this
         Contact bob = new Contact("Bob", 30);
         Contact alice = new Contact("Alice", 20);
         Contact charlie = new Contact("Charlie", 25);
+        Contact alice2 = new Contact("Alice", 10);
 
         ArrayList<Contact> contacts = new ArrayList();  //Create an ArrayList called contacts
         contacts.add(bob);
         contacts.add(alice);
         contacts.add(charlie);
+        contacts.add(alice2);
+
+        Collections.sort(contacts);
 
         ArrayList<Contact> contactsUnderThirty = new ArrayList();
 
