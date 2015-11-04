2e9bbf6e0affc4f5ff4d22faf56d77337d7507d2
JackNel <jcneligan@gmail.com>
2015-11-04 10:13:55 -0500

    Add Exercise9, example of alphabetically sorting using compareTo()

---
+++ src/Exercise9.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Jack on 11/4/15.
+ */
+
+// Example of alphabetically sorting using compareTo()
+public class Exercise9 {
+    public static void main(String[] args) {
+        String[] names = {"Charlie", "David", "Alice", "Bob"};
+        ArrayList<String> sortedNames = new ArrayList();
+        for (String name : names) {
+            int index = 0;
+            for (String sortedName : sortedNames) {
+                if (name.compareTo(sortedName) > 0) {
+                    index++;
+                }
+                else {
+                    break;
+                }
+            }
+            sortedNames.add(index, name);
+        }
+        System.out.println(sortedNames);
+    }
+}
